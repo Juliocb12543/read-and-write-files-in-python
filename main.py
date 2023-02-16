6d3dedc49debb466a640039ba30c4c59bb541539
Julio Barrientos <113930475+Juliocb12543@users.noreply.github.com>
2023-02-16 18:40:23 +0000

    wrote text files

---
+++ main.py
@@ -0,0 +1,21 @@
+# # remember to fork this and push to github
+
+# f = open('test.txt','a')
+# f.write("tmr is friday")
+# # read gives us everything
+# # readline gives us the first line
+# # print(f.read())
+# f.close()
+
+# f = open('employees.txt', 'a')
+# f.write("Juan - salesman")
+# f.close()
+
+# with open("test.txt", 'r') as f:
+#   # f.write("hfbfhbjbvhkbefvjkbkfjbjenwvjnjvnlqnrjvjwenwen")
+#   # print(f)
+#   for line in f:
+#     print(line, end='')
+
+with open('period7.txt', 'a') as f:
+  f.write("we finished appending text")
